ee58d3d5cffe2491d94a62c5ef4306f05e24f4c4
BK <58073787+akaitrade@users.noreply.github.com>
2024-06-28 11:22:49 +0200

    Init

---
+++ SetIpShell.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Check if the correct number of arguments are passed
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <path_to_yml_file> <new_WG_HOST_value>"
+    exit 1
+fi
+
+# Assign arguments to variables
+YML_FILE=$1
+NEW_WG_HOST_VALUE=$2
+
+# Use sed to update the WG_HOST value
+sed -i.bak -E "s/(WG_HOST: ).*/\1$NEW_WG_HOST_VALUE/" $YML_FILE
+
+# Check if sed command was successful
+if [ $? -eq 0 ]; then
+    echo "Successfully updated WG_HOST value in $YML_FILE"
+else
+    echo "Failed to update WG_HOST value in $YML_FILE"
+    exit 1
+fi
